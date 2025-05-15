17c16a4652aad28b91b6a753630512dbf7b2ca32
kingzainab <zainabasif14@gmail.com>
2025-05-15 12:08:52 +0500

    changes

--- vercel demo/hello.js
+++ vercel demo/hello.js
@@ -1,5 +1,6 @@
 export default function handler(req, res) {
-    setTimeout(() => {
-      res.status(200).send("Hello from GitHub to Vercel!");
-    }, 1000);
+  setTimeout(() => 
+    {
+	    res.status(200).send("Hello from Vercel! Auto-scaling triggered.");
+    }, 1000); // Simulate 1s processing time
   }
