2fdd96e89ada4469b695e6fae398d80234b3ee22
kingzainab <zainabasif14@gmail.com>
2025-05-22 12:50:43 +0500

    22/05/2025 Lab Commit

--- api/hello.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  setTimeout(() => 
-    {
-	    res.status(200).send("Hello from Vercel! Auto-scaling triggered.");
-    }, 1000); // Simulate 1s processing time
-  }
